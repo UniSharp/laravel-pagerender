5d0fc1e95999e2dfca93ed1a3ffcd9cebf16d2ae
Stream <stream@unisharp.com>
2015-10-04 17:18:24 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# Page Render Package for Laravel
+
+ * Simple key-value storage
+ * Support multi-level array (dot delimited keys) structure.
+ * Localization supported.
+
+## Installation
+
+1. install package
+
+	```php
+		composer require unisharp/laravel-pagerender
+	```
+
+1. use the trait in your model
+
+	```php
+		use \Unisharp\Pagerender\Pagerender;
+	```
+
+1. make sure your table has these columns : `parent_id`, `alias`, `custom_view`
+
+## Usage
+
+```php
+	$page = new Page();
+
+	$page->render();
+	// Generates the default view(or custom view if the column is not empty).
+
+	$page->subs;
+	// Get children pages.
+
+	$page->hasSubs();
+	// Check if children pages exist.
+
+	$page->parent;
+	// Get parent page.
+
+	$page->hasParent();
+	// Check if parent page exists.
+
+	$page->roots();
+	// Get all pages at top level.
+
+	$page->isRoot();
+	// Check if this page is at top level.
+
+	$page->getByAlias('aboutus');
+	// Get the about us page.
+
+    $page->hasByAlias($alias);
+    // Check if the about us page exists.
+
+    $page->allWithAlias();
+    // Get pages that have alias.
+
+    $page->getLevel();
+    // Get level count(top level as 0).
+
+    $page->ancestors();
+    // Get all parent pages of the current page.
+```
+
+## Todo
+
+```php
+	$page->tree();
+	// Get all pages with parent-child structure.
+```
---
+++ src/Pagerender/Pagerender.php
@@ -0,0 +1,110 @@
+<?php
+namespace Unisharp\Pagerender;
+
+trait Pagerender
+{
+    /*********************************
+     *********    General    *********
+     ********************************/
+
+    public function render()
+    {
+    	if (!empty($page->custom_view)) {
+    	    return view('page.'.$page->custom_view);
+    	} else {
+    	    return view('page.show');
+    	}
+    }
+
+	public function subs()
+    {
+        return $this->hasMany(get_class(), 'parent_id', 'id');
+    }
+
+    public function hasSubs()
+    {
+    	return $this->subs->count > 0;
+    }
+
+    public function parent()
+    {
+        return $this->hasOne(get_class(), 'id', 'parent_id');
+    }
+
+    public function hasParent()
+    {
+    	if ($this->parent_id === 0) {
+    		return false;
+    	} else {
+    		return true;
+    	}
+    }
+
+    public function roots()
+    {
+    	return self::where('parent_id', 0)->get();
+    }
+
+    public function isRoot()
+    {
+    	return !self::hasParent();
+    }
+
+    /*********************************
+     **********    Alias    **********
+     ********************************/
+
+    public function getByAlias($alias)
+    {
+    	return self::where('alias', $alias)->first();
+    }
+
+    public function hasByAlias($alias)
+    {
+    	return self::where('alias', $alias)->exists();
+    }
+
+    public function allWithAlias()
+    {
+    	return self::whereNotNull('alias')->get();
+    }
+
+    /********************************
+     **********    Tree    **********
+     *******************************/
+
+    public function getLevel($page = null)
+    {
+    	if (is_null($page)) {
+    		$page = $this->find($this->id);
+    	}
+
+    	if ($page->isRoot()) {
+            return 0;
+        }
+
+        return 1 + self::getLevel($page->parent);
+    }
+
+    public function ancestors($page = null, $arr_page = array())
+    {
+    	if (is_null($page)) {
+    		$page = $this->find($this->id);
+    	}
+
+    	if ($page->isRoot()) {
+			array_push($arr_page, $page);
+            return $arr_page;
+        }
+
+		$arr_page = self::ancestors($page->parent, $arr_page);
+		array_push($arr_page, $page);
+
+        return $arr_page;
+    }
+
+    public function tree()
+    {
+    	return true;
+    }
+}
