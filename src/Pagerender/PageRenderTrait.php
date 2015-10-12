5eff2b85770abd8409b1635c6c982220467fbeba
StreamHuang <g0110280@gmail.com>
2015-10-12 11:31:59 +0800

    fix has parent check

--- src/Pagerender/PageRenderTrait.php
+++ src/Pagerender/PageRenderTrait.php
@@ -33,7 +33,7 @@ trait PageRenderTrait
 
     public function hasParent()
     {
-    	if ($this->parent_id === 0) {
+    	if ($this->parent_id === 0 || is_null($this->parent_id)) {
     		return false;
     	} else {
     		return true;
